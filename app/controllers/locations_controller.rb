eed381f53ebf8443a361a5fbfd3141c9176cc33f
juan solano <juanpablosolanosuarez@hotmail.com>
2020-01-22 18:10:00 +0100

    Location#new working

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -7,15 +7,26 @@ class LocationsController < ApplicationController
   end
 
   def new
+   @location = Location.new
+  end
 
+  def create
+   @location = Location.create(review_params)
   end
 
   def edit
+
   end
 
   def update
+
   end
 
   def destroy
+
+  end
+
+    def review_params
+    params.require(:location).permit(:name,:description)
   end
 end
